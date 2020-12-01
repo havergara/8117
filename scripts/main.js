181dc98b1d2762a811df8781210ad66071b4e09b
Henzon Vergara <henzonvergara@yahoo.com>
2020-11-30 23:12:47 -0500

    'userValidation'

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,29 @@
 $(function (){
 
     $('.mobileNav, .parentNav').on('click', function (){
-        if($(this).next('ul').is(":visible")){
-            $(this).next('ul').slideUp('slow');
-        } else{
-            $(this).next('ul').slideDown('slow');
+        if (sessionStorage.getItem("userToken") !== null) {
+            if($(this).next('ul').is(":visible")){
+                $(this).next('ul').slideUp('slow');
+            } else{
+                $(this).next('ul').slideDown('slow');
+            }
+        } else {
+            window.location.href = 'login.html';
         }
     });
+
+    if (sessionStorage.getItem("userToken") === null) {
+        document.querySelector('.subNav').style.display = 'none';
+    } else {
+        document.querySelector('#loginLink').style.display = 'none';
+        document.querySelector('#signUpLink').style.display = 'none';
+
+        document.querySelector('#logoutLink').style.display = 'block';
+    }
+
+    logoutLink.firstChild.addEventListener('click', function () {
+        sessionStorage.clear();
+        window.location.href = 'index.html';
+    })
 
 })
