ec71036b85ac53650ebd6de0034ef67029b45412
sukidawood <63749531+sukidawood@users.noreply.github.com>
2020-12-02 19:54:44 -0500

    dashboard.html

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,3 +1,5 @@
+
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -19,10 +21,47 @@ for (i = 0; i < coll.length; i++) {
 //   }
 // }
 
-document.querySelector('select').onchange=clickable;
-var line1 = document.getElementById("line1");
-function clickable(event){
-  line1.classList = this.value;
-}
+// let bgcolor = function(){
+//     document.querySelector('select').onchange=clickable;
+//     var line1 = document.getElementById("line1");
+//     function clickable(event){
+//     line1.classList = this.value;
+// }
+
+// }
+
+
+
+
+// $(document).ready(function(){
+//   $("select[value = 'joyful']").on({
+//     click: function(){
+//       $(this).css('background-color', "yellow")
+//     }
+//   });
+// });
+
+$("select[name='moods']").change(function(){
+    if ($(this).val() == "joyful"){
+        $(this).css('background-color',"yellow");
+  }
+    if ($(this).val() == "sad"){
+        $(this).css('background-color',"var(--blue)");
+  }
+    if ($(this).val() == "disgusted"){
+        $(this).css('background-color',"green");
+  }
+    if ($(this).val() == "average"){
+        $(this).css('background-color',"wheat");
+  }
+
+    if ($(this).val() == "angry"){
+        $(this).css('background-color',"red");
+  }
+
+    if ($(this).val() == "nervous"){
+        $(this).css('background-color',"violet");
+  }
+});
 
 
